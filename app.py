0a42312d28aaadfd52b26d5a37d702bc6ff86e12
Stuti <stuti.d@gmail.com>
2015-07-26 21:35:52 +0800

    testing type of routes

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask
 # create the applications object
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
 #use decorators to link the function to a URL
 
 @app.route("/")
@@ -14,9 +15,36 @@ app = Flask(__name__)
 # define the view using a function, which returns a string
 
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!??!!!"
 
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
 # start the dev server using run()
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value+1
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value+1
+    return "correct"
+
+#dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
 
 if __name__ == "__main__":
     app.run()
